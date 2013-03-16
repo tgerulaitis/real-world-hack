6fc4174b54d715da385b748004227dbc3c7fae30
David Kelly <Dave@Davids-MacBook-Pro-3.local>
2013-03-16 17:05:36 +0000

    Design additions

---
+++ design.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Frabble</title>
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+  
+
+  <script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+	<div class="row">
+		<div class="large-12 columns">
+		  <div class="header">
+  			<h1 class="logo">Fra<span>bble</span><span class="beta"> beta</span></h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div class="block">
+  	    <h4 class="subheader">Guess the movie title</h4>
+  	    <a href="" class="try right">Try another</a>
+  	    <ul class="large-block-grid-6 small-block-grid-3">
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>  	        	        	        	        	      
+  	    </ul>  	    
+  	  </div>
+  	</div>
+	</div>
+	
+	<div class="row">
+  	    <div class="large-12 columns">
+  	    <div class="block">
+  	    <h4 class="subheader">Your answer</h4>
+              <div class="row collapse">
+                <div class="small-10 columns">
+                  <input type="text" placeholder="Movie title...">
+                </div>
+                <div class="small-2 columns">
+                  <a href="#" class="button prefix">Guess</a>
+                </div>
+              </div>
+            </div> 
+  	    </div>
+	</div>
+	
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div class="block">
+  	   <h4 class="subheader">Need a hint?</h4>
+  	   <hr />
+  	       <ul class="icons small-block-grid-1 large-block-grid-2">
+    	       <li id="actor">
+	    	       <strong>Staring:</strong>
+	    	       <a href="" class="try">Reveal hint</a>
+	    	       <span class="hidden"  John Smith</span>
+    	       </li>
+    	       <li id="director">
+	    	       <strong>Director:</strong>
+	    	       <a href="" class="try">Reveal hint</a>
+	    	       <span class="hidden"  John Smith</span>
+    	       </li>
+    	       <li id="year">
+	    	       <strong>Year:</strong>
+	    	       <a href="" class="try">Reveal hint</a>
+	    	       <span class="hidden" 2009</span>
+    	       </li>
+    	       <li id="genre">
+	    	       <strong>Genre:</strong>
+	    	       <a href="" class="try">Reveal hint</a>
+	    	       <span class="hidden"  Horror</span>
+    	       </li>
+  	       </ul>
+  	  </div>
+  	</div>
+	</div>
+
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div class="footer">Made by <a href="http://stormconsultancy.co.uk">Storm</a> &amp; Friends at <a href="http://realworldstudios.com/">Real World Studios</a> as part of <a href="http://bathdigitalfestival.com">Bath Digital Festival</a>
+  	  </div>
+  	</div>
+	</div>
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="js/foundation.min.js"></script>
+  <!--
+  
+  <script src="js/foundation/foundation.js"></script>
+  
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  
+  <script src="js/foundation/foundation.forms.js"></script>
+  
+  <script src="js/foundation/foundation.alerts.js"></script>
+  
+  <script src="js/foundation/foundation.magellan.js"></script>
+  
+  <script src="js/foundation/foundation.reveal.js"></script>
+  
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  
+  <script src="js/foundation/foundation.clearing.js"></script>
+  
+  <script src="js/foundation/foundation.cookie.js"></script>
+  
+  <script src="js/foundation/foundation.joyride.js"></script>
+  
+  <script src="js/foundation/foundation.orbit.js"></script>
+  
+  <script src="js/foundation/foundation.section.js"></script>
+  
+  <script src="js/foundation/foundation.topbar.js"></script> 
+  -->
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
