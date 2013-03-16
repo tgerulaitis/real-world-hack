2fdb059b9ea50ff90482e96eab77535eae90ed64
tgerulaitis <t.gerulaitis@gmail.com>
2013-03-16 23:05:20 +0000

    Add guessMovie and revealHint server side ajax methods

--- ajax.php
+++ ajax.php
@@ -6,7 +6,13 @@ if (isset($_POST['method'])) $_GET = $_POST;
 switch ($_GET['method']) {
 	case 'getNewMovie':
 		getNewMovie();
-	  break;
+	    break;
+    case 'guessMovie':
+        guessMovie();
+        break;
+    case 'revealHint':
+        revealHint();
+        break;
 	default:
 		break;
 }
@@ -59,6 +65,64 @@ function getNewMovie() {
 	
 	die(json_encode(array('result'=>'success','words'=>$display)));
 	
+}
+
+function guessMovie() {
+    if (!isset($_SESSION['movie'])) {
+        die(json_encode(array('result'=>'failure','error'=>'No movie chosen')));
+    }
+    if (!isset($_GET['answer'])) {
+        die(json_encode(array('result'=>'failure','error'=>'No answer supplied')));
+    }
+    
+    $movie = $_SESSION['movie'];
+    $answer = $_GET['answer'];
+    
+    if (strtolower($answer) == strtolower($movie['title'])) {
+        unset($_SESSION['movie']);
+        die(json_encode(array('result'=>'success','correct'=>true,'title'=>$movie['title'])));
+    } else {
+        die(json_encode(array('result'=>'success','correct'=>false)));
+    }  
+}
+
+function revealHint() {
+    if (!isset($_SESSION['movie'])) {
+        die(json_encode(array('result'=>'failure','error'=>'No movie chosen')));
+    }
+    if (!isset($_GET['type'])) {
+        die(json_encode(array('result'=>'failure','error'=>'No hint type supplied')));
+    }
+    
+    $movie = $_SESSION['movie'];
+    $type = $_GET['type'];
+    $value = "";
+    
+    switch($type) {
+        case 'starring':
+            $value = $movie['metadata']['item']['actors']['actor'][0];
+            break;
+        case 'director':
+            $value = $movie['metadata']['item']['directors']['director'];
+            if (is_array($value)) {
+                $value = $value[0];
+            }
+            break;
+        case 'year':
+            $value = $movie['metadata']['item']['releaseYear'];
+            break;
+        case 'genre':
+            $value = $movie['metadata']['item']['genres']['genre'];
+            if (is_array($value)) {
+                $value = implode(', ', $value);
+            }
+            break;
+        default:
+            die(json_encode(array('result'=>'failure','error'=>'No such hint type')));
+            break;
+    }
+    
+    die(json_encode(array('result'=>'success','hint'=>array('type'=>$type,'value'=>$value))));
 }
 
 ?>
