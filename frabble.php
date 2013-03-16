61414023433c7109f482f9546059623f2b13d9ea
Liam Gladdy <liam@gladdy.co.uk>
2013-03-16 14:25:51 +0000

    Adding local

---
+++ frabble.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    
+
+    <?php require('header.php'); ?>
+    
+    <h1>Local Frabble</h1>
+    
+    <?php require('footer.php'); ?>
+  </body>
+</html>
