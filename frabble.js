c710a104d57ad9ab0259569f50125ffa3ada8356
David Kelly <dave@stormconsultancy.co.uk>
2013-03-18 12:34:12 +0000

    latest changes

--- frabble.js
+++ frabble.js
@@ -1,11 +1,115 @@
 $(document).ready(function() {
-	$('#start').click(function() {
+	$('#start').click(function(e) {
 		startGame();
+		e.preventDefault();
 	});
+	
+	$('#answer').click(function(e) {
+		testAnswer();
+		e.preventDefault();
+	});
+	
+	$('#actorhint').click(function(e) {
+		actorHint();
+		e.preventDefault();
+	});
+	
+	$('#directorhint').click(function(e) {
+		directorHint();
+		e.preventDefault();
+	});
+	
+	$('#yearhint').click(function(e) {
+		yearHint();
+		e.preventDefault();
+	});
+	
+	$('#genrehint').click(function(e) {
+		genreHint();
+		e.preventDefault();
+	});
+	
+	
 });
 
 function startGame() {
-	alert("Starting the game");
-	
-	
+
+  	    $.getJSON('http://frabble.local/ajax.php?method=getNewMovie', function(data) {
+	  	    var items = [];
+	  	    words = data.words;
+	  	    $.each(words, function(index, word) {
+	  	    if(word.type == 'text'){
+	  	    	items.push('<li class="'+ word.type +'">' + word.result + '</li>');
+	  	    }
+	  	    else{
+	  	    	items.push('<li class="'+ word.type +'"><img src="' + word.result + '" /></li>');
+		  	}
+		  	});
+		  	$('#imagelist').html(items.join(''));
+	  	});
+	  	$('#theanswer').replaceWith('<div id="theanswer" class="block">*drum roll....*</div>');
+	  	$('#actorhintval').empty();
+	  	$('#directorhintval').empty();
+	  	$('#yearhintval').empty();
+	  	$('#genrehintval').empty();
+	  	
+	  	
+	  	$('#actorhint').show();
+	  	$('#directorhint').show();
+	  	$('#yearhint').show();
+	  	$('#genrehint').show();
+
+		return(false);	  	
 }
+
+
+function testAnswer() {
+
+		var thefinalanswer = $('#myanswer').val();		
+  	    $.getJSON('http://frabble.local/ajax.php?method=guessMovie&answer='+thefinalanswer, function(data) {
+	  	    var items = [];
+	  	    if(data.correct == true){
+	  	    	items.push('Correct!');
+	  	    	$('#theanswer').addClass("winner");
+	  	    }
+	  	    else{
+	  	    	items.push('Try Again...');
+		  	}
+		  	$('#theanswer').html(items.join(''));
+	  	});
+
+		return(false);	  	
+}
+
+function actorHint() {	
+  	    $.getJSON('http://frabble.local/ajax.php?method=revealHint&type=starring', function(data) {
+		  	$('#actorhintval').html(data.hint.value);
+		  	$('#actorhint').hide();
+	  	});
+		return(false);	  	
+}
+
+function yearHint() {	
+  	    $.getJSON('http://frabble.local/ajax.php?method=revealHint&type=year', function(data) {
+		  	$('#yearhintval').html(data.hint.value);
+		  	$('#yearhint').hide();
+	  	});
+		return(false);	  	
+}
+
+function directorHint() {	
+  	    $.getJSON('http://frabble.local/ajax.php?method=revealHint&type=director', function(data) {
+		  	$('#directorhintval').html(data.hint.value);
+		  	$('#directorhint').hide();
+	  	});
+		return(false);	  	
+}
+
+function genreHint() {	
+  	    $.getJSON('http://frabble.local/ajax.php?method=revealHint&type=genre', function(data) {
+		  	$('#genrehintval').html(data.hint.value);
+		  	$('#genrehint').hide();
+	  	});
+		return(false);	  	
+}
+
--- frabble.php
+++ frabble.php
@@ -2,23 +2,155 @@
 require('functions.php');
 
 ?>
+
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="frabble.js"></script>
-	</head>
-	<body>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Frabble</title>
+  <link rel="stylesheet" href="stylesheets/normalize.css" />
+  <link rel="stylesheet" href="stylesheets/foundation.css" />
+  <link rel="stylesheet" href="stylesheets/style.css" />
+  <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="frabble.js"></script>
+  <script src="javascript/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+<?php require('header.php'); ?>
+	<div class="row">
+		<div class="large-12 columns">
+		  <div class="header">
+  			<h1 class="logo">Fra<span>bble</span><span class="beta"> beta</span></h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div class="block">
+  	    <h4 class="subheader">Guess the movie title</h4>
+  	    <a href="#" class="try right" id="start">Try another</a>
+
+  	    <ul class="large-block-grid-6 small-block-grid-3" id="imagelist">
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>
+  	      <li><img src="http://placehold.it/100x100"/></li>  	        	        	        	        	      
+  	    </ul>  	    
+  	  </div>
+  	</div>
+	</div>
 	
-		<?php require('header.php'); ?>
-		
-		<h1>Local Frabble</h1>
-		
-		<a href="#" id="start">Start</a>
-		
-		
-		
-		<?php require('footer.php'); ?>
-	</body>
+	<div class="row">
+  	    <div class="large-12 columns">
+  	    <div class="block">
+  	    <h4 class="subheader">Your answer</h4>
+              <div class="row collapse">
+                <div class="small-10 columns">
+                  <input id="myanswer" type="text" placeholder="Movie title...">
+                </div>
+                <div class="small-2 columns">
+                  <a href="#" class="button prefix" id="answer">Guess</a>
+                </div>
+              </div>
+            </div>
+  	    </div>
+	</div>
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div id="theanswer" class="block">
+  	   *drum roll....*
+  	  </div>
+  	</div>
+	</div>
+	
+	
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div class="block">
+  	   <h4 class="subheader">Need a hint?</h4>
+  	   <hr />
+  	       <ul class="icons small-block-grid-1 large-block-grid-2">
+    	       <li id="actor">
+	    	       <strong>Staring:</strong>
+	    	       <a href="#" id="actorhint" class="try">Reveal hint</a>
+	    	       <span id="actorhintval" class="hidden"></span>
+    	       </li>
+    	       <li id="director">
+	    	       <strong>Director:</strong>
+	    	       <a href="#" id="directorhint" class="try">Reveal hint</a>
+	    	       <span id="directorhintval" class="hidden"></span>
+    	       </li>
+    	       <li id="year">
+	    	       <strong>Year:</strong>
+	    	       <a href="#" id="yearhint" class="try">Reveal hint</a>
+	    	       <span id="yearhintval" class="hidden"></span>
+    	       </li>
+    	       <li id="genre">
+	    	       <strong>Genre:</strong>
+	    	       <a href="#" id="genrehint" class="try">Reveal hint</a>
+	    	       <span id="genrehintval" class="hidden"></span>
+    	       </li>
+  	       </ul>
+  	  </div>
+  	</div>
+	</div>
+
+	<div class="row">
+  	<div class="large-12 columns">
+  	  <div class="footer">Made by <a href="http://stormconsultancy.co.uk">Storm</a> &amp; Friends at <a href="http://realworldstudios.com/">Real World Studios</a> as part of <a href="http://bathdigitalfestival.com">Bath Digital Festival</a>
+  	  </div>
+  	</div>
+	</div>
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="js/foundation.min.js"></script>
+  <!--
+  
+  <script src="js/foundation/foundation.js"></script>
+  
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  
+  <script src="js/foundation/foundation.forms.js"></script>
+  
+  <script src="js/foundation/foundation.alerts.js"></script>
+  
+  <script src="js/foundation/foundation.magellan.js"></script>
+  
+  <script src="js/foundation/foundation.reveal.js"></script>
+  
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  
+  <script src="js/foundation/foundation.clearing.js"></script>
+  
+  <script src="js/foundation/foundation.cookie.js"></script>
+  
+  <script src="js/foundation/foundation.joyride.js"></script>
+  
+  <script src="js/foundation/foundation.orbit.js"></script>
+  
+  <script src="js/foundation/foundation.section.js"></script>
+  
+  <script src="js/foundation/foundation.topbar.js"></script> 
+  -->
+  <?php require('footer.php'); ?>
+  <script>
+    $(document).foundation();
+  </script>
+</body>
 </html>
